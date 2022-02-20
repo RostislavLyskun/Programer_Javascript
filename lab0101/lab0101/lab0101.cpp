da933e2d230728444376c8249efcb8450695f5de
RostislavLyskun <99643719+RostislavLyskun@users.noreply.github.com>
2022-02-20 14:03:21 +0200

    Create lab0101.cpp
    lab01_01

---
+++ lab0101/lab0101/lab0101.cpp
@@ -0,0 +1,23 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double A, B;
+
+    cin >> A;
+    cin >> B;
+
+    double S = A + B;
+    double K = A - B;
+    double C = A * B;
+    double Z = A / B;
+
+    cout << "A + B = " << S << endl;
+    cout << "K - B = " << K << endl;
+    cout << "A * B = " << C << endl;
+    cout << "K / B = " << Z << endl;
+}
+
